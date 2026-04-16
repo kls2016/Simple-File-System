0795e6cb5cbc97904363dec65bec7a26d91c2e4d
K L Shishir <shishir.kuppahalli.lakshmisha@continental-corporation.com>
2026-04-16 14:16:14 +0530

    Added insertion sort

--- test.c
+++ test.c
@@ -1956,3 +1956,18 @@ int ternarySearch(int arr[], int left, int right, int key) {
     }
     return -1; // not found
 }
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];   // element to insert
+        int j = i - 1;
+
+        // Move elements greater than key one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key; // place key in correct position
+    }
+}
