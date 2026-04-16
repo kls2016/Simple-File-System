cbf270ee6d6b10c030413491686f5dbfa501eb2a
K L Shishir <shishir.kuppahalli.lakshmisha@continental-corporation.com>
2026-04-16 11:01:17 +0530

    Added quicksort()

--- test.c
+++ test.c
@@ -1858,3 +1858,36 @@ void mergeSort(int arr[], int left, int right) {
     }
 }
 
+// Swap utility
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Partition function (Lomuto)
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];  // choose last element as pivot
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+// QuickSort function
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);  // left side
+        quickSort(arr, pi + 1, high); // right side
+    }
+}
+
