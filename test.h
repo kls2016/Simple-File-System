e0b8925ab9c3630a3d4db840bc7299975293bf26
K L Shishir <shishir.kuppahalli.lakshmisha@continental-corporation.com>
2026-04-20 13:14:02 +0530

    cleaned up test.c and test.h

--- test.h
+++ test.h
@@ -2,11 +2,22 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "C:\\LegacyApp\\cmocka\\include\\cmocka.h"
+// #include "C:\\LegacyApp\\cmocka\\include\\cmocka.h"
+
+#ifndef TEST_H
+#define TEST_H
 
 int run_tests_main(void);
 
 #define MAX_SIZE 100
+
+struct TreeNode {
+    int value;
+    struct TreeNode* leftChild;
+    struct TreeNode* rightChild;
+};
+
+/* Function declarations */
 
 typedef struct {
     int items[MAX_SIZE];
@@ -14,28 +25,10 @@ typedef struct {
 } Stack;
 
 
-long long factorial(int n);
-bool is_prime(int n);
-size_t safe_strlen(const char *str);
-int is_sorted(int arr[], int size);
-void bubble_sort(int arr[], int size);
-void swap(int *a, int *b);
-int is_bit_set(int n, int pos);
-int toggle_bit(int n, int pos);
-int clear_bit(int n, int pos);
-int set_bit(int n, int pos);
-int is_even(int n);
-int my_strcmp(const char *s1, const char *s2);
-char* my_strcpy(char *dest, const char *src);
-size_t my_strlen(const char *str);
-char* read_from_file(const char *filename);
-int append_to_file(const char *filename, const char *content);
-int write_to_file(const char *filename, const char *content);
-int safe_binary_search(int arr[], int size, int target);
-int binary_search(int arr[], int size, int target);
-int peek(Stack *s, int *topValue);
-int pop(Stack *s, int *poppedValue);
-int push(Stack *s, int value);
-int isFull(Stack *s);
-int isEmpty(Stack *s);
-void initStack(Stack *s);
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+double calculate(double a, double b, char operator);
+
+#endif	// TEST_H
