a9ec4005e8687f493b1801163d7a86083cdaf846
K L Shishir <shishir.kuppahalli.lakshmisha@continental-corporation.com>
2026-04-24 17:39:00 +0530

    Merge Sort

--- test.h
+++ test.h
@@ -25,19 +25,7 @@ typedef struct {
 } Stack;
 
 
-double addit(double a, double b);
-double subtractit(double a, double b);
-double multiplyit(double a, double b);
-double divideit(double a, double b);
-double calculateit(double a, double b, char operator);
 void merge(int arr[], int left, int mid, int right);
 void mergeSort(int arr[], int left, int right);
-void swap(int *a, int *b);
-int partition(int arr[], int low, int high);
-void quickSort(int arr[], int low, int high);
-void heapify(int arr[], int n, int i);
-void heapSort(int arr[], int n);
-void bubbleSort(int arr[], int n);
-void selectionSort(int arr[], int n);
 
 #endif	// TEST_H
