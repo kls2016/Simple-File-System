1faf34a8c098fc67e7cb72828d73aaf334216809
K L Shishir <shishir.kuppahalli.lakshmisha@continental-corporation.com>
2026-04-26 18:21:36 +0530

    Quicksort

--- test.h
+++ test.h
@@ -31,5 +31,7 @@ void bubbleSort(int arr[], int n);
 void heapify(int arr[], int n, int i);
 void heapSort(int arr[], int n);
 void swap(int *a, int *b);
+int partition(int arr[], int low, int high);
+void quickSort(int arr[], int low, int high);
 
 #endif	// TEST_H
