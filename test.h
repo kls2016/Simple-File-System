85b6737741f870d42868c2874afdaf0325eb2df9
K L Shishir <shishir.kuppahalli.lakshmisha@continental-corporation.com>
2026-04-27 16:24:26 +0530

    Qsort

--- test.h
+++ test.h
@@ -12,5 +12,8 @@ int run_tests_main(void);
 #define MAX_SIZE 100
 
 void bubbleSort(int arr[], int n);
+void swap(int *a, int *b);
+int partition(int arr[], int low, int high);
+void quickSort(int arr[], int low, int high);
 
 #endif	// TEST_H
