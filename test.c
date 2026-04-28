ef1d931d6707ffe1bf543a57d6b55115eb55fd86
K L Shishir <shishir.kuppahalli.lakshmisha@continental-corporation.com>
2026-04-28 12:49:45 +0530

    ISort

--- test.c
+++ test.c
@@ -385,5 +385,40 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
-#include <stdio.h>
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+		int maxIndex = n - 1;
+
+        // Find the minimum element in remaining array
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap the found minimum with first element
+        if (minIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];   // element to insert
+        int j = i - 1;
+
+        // Move elements greater than key one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key; // place key in correct position
+    }
+}
+
 
