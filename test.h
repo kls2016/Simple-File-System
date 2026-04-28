f0ed5b6512c355d02f647753b15b16d712038b85
K L Shishir <shishir.kuppahalli.lakshmisha@continental-corporation.com>
2026-04-28 18:32:08 +0530

    Reset

--- test.h
+++ test.h
@@ -11,13 +11,5 @@ int run_tests_main(void);
 
 #define MAX_SIZE 100
 
-void merge(int arr[], int left, int mid, int right);
-void mergeSort(int arr[], int left, int right);
-void swap(int *a, int *b);
-int partition(int arr[], int low, int high);
-void quickSort(int arr[], int low, int high);
-void heapify(int arr[], int n, int i);
-void heapSort(int arr[], int n);
-void bubbleSort(int arr[], int n);
 
 #endif	// TEST_H
