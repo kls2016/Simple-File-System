874233d586dce50df3c535160869e1df14817144
K L Shishir <shishir.kuppahalli.lakshmisha@continental-corporation.com>
2026-05-08 19:50:51 +0530

    BS removed

--- test.c
+++ test.c
@@ -456,22 +456,4 @@ void quickSort(int arr[], int low, int high) {
     }
 }
 
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int swapped = 0; // optimization flag
 
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // swap
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                swapped = 1;
-            }
-        }
-
-        // If no swaps happened, array is already sorted
-        if (swapped == 0)
-            break;
-    }
-}
