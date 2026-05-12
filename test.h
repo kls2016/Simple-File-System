be280cb6e1c9cc032b2e192f9f265ac34cdf19ca
K L Shishir <shishir.kuppahalli.lakshmisha@continental-corporation.com>
2026-05-12 16:59:25 +0530

    Algos rem

--- test.h
+++ test.h
@@ -16,9 +16,5 @@ void mergeSort(int arr[], int left, int right);
 void swap(int *a, int *b);
 int partition(int arr[], int low, int high);
 void quickSort(int arr[], int low, int high);
-void bubbleSort(int arr[], int n);
-void selectionSort(int arr[], int n);
-void insertionSort(int arr[], int n);
-int ternarySearch(int arr[], int left, int right, int key);
 
 #endif	// TEST_H
