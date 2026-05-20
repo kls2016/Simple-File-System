c548f526d214487c93fd1eac7db2545316eb7ab5
K L Shishir <shishir.kuppahalli.lakshmisha@continental-corporation.com>
2026-05-20 17:44:22 +0530

    Removed algos

--- test.h
+++ test.h
@@ -11,14 +11,5 @@ int run_tests_main(void);
 
 #define MAX_SIZE 100
 
-void merge(int arr[], int left, int mid, int right);
-void mergeSort(int arr[], int left, int right);
-void swap(int *a, int *b);
-int partition(int arr[], int low, int high);
-void quickSort(int arr[], int low, int high);
-void bubbleSort(int arr[], int n);
-void selectionSort(int arr[], int n);
-void insertionSort(int arr[], int n);
-int ternarySearch(int arr[], int left, int right, int key);
 
 #endif	// TEST_H
