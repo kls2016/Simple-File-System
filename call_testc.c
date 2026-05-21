21053b5a7af980eeda04b2ea806d81f750bc0150
K L Shishir <shishir.kuppahalli.lakshmisha@continental-corporation.com>
2026-05-21 10:29:34 +0530

    removed fns

--- call_testc.c
+++ call_testc.c
@@ -1,56 +1,6 @@
 #include <stdio.h>
 #include <alloc.h>
 
-#include "test.h"
 
 
-int *create_array(int len)
-{
-	int *ar = malloc(len*sizeof(int));
 
-	if(ar == NULL)
-	{
-		printf("Error array not created\n");
-		return NULL;
-	}
-	else
-		return ar;
-}
-
-
-void select_algo(int choice)
-{
-	int *ar;
-	int n = 10;
-	
-	ar = create_array(n);
-
-	if(ar == NULL)
-	{
-		printf("\nGot Empty Array! Not calling sorting algos\n");
-		return;
-	}
-
-	switch(choice)
-	{
-		case 1:
-			bubbleSort(ar, n);
-			break;
-
-		case 2:
-			quickSort(ar, 0,  n);
-			break;
-
-		case 3:
-			heapSort(ar, n);
-			break;
-
-		case 4:
-			mergeSort(ar, 0, n);
-			break;
-
-		default:
-			printf("Error, wrong selection\n");
-			break;
-	}
-}
